50d74b07d4537ef4f612195e2bc454d786d6c928
Becca <becca.hunt83@gmail.com>
2017-11-14 15:40:53 +0000

    Layout of the form

--- turingMachine/turingMachine/Form1.Designer.cs
+++ turingMachine/turingMachine/Form1.Designer.cs
@@ -28,12 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Tape2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Tape1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // Tape2
+            // 
+            this.Tape2.Location = new System.Drawing.Point(21, 128);
+            this.Tape2.Name = "Tape2";
+            this.Tape2.Size = new System.Drawing.Size(852, 94);
+            this.Tape2.TabIndex = 2;
+            // 
+            // Tape1
+            // 
+            this.Tape1.Location = new System.Drawing.Point(21, 25);
+            this.Tape1.Name = "Tape1";
+            this.Tape1.Size = new System.Drawing.Size(852, 86);
+            this.Tape1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 392);
+            this.Controls.Add(this.Tape1);
+            this.Controls.Add(this.Tape2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel Tape2;
+        private System.Windows.Forms.FlowLayoutPanel Tape1;
     }
 }
 
--- turingMachine/turingMachine/Form1.cs
+++ turingMachine/turingMachine/Form1.cs
@@ -16,5 +16,80 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            createTape();
+            createTape2();
+
+        }
+
+        private void createTape()
+        {
+            int n = 3;
+            int lSize = 35;
+
+            Label[] label = new Label[n];
+
+            for (int i = 0; i < label.Length; i++)
+            {
+                label[i] = new Label();
+                label[i].AutoSize = false;
+                label[i].BorderStyle = BorderStyle.FixedSingle;
+                label[i].Size = new Size(lSize, lSize);
+                label[i].TextAlign = ContentAlignment.MiddleCenter;
+                label[i].Font = new Font(label[i].Font.Name, 18F);
+                label[i].Name = "n" + i;
+                label[i].Text = "" + i;
+                Tape1.Controls.Add(label[i]);
+
+                label[i].Click += new EventHandler(label_clickas);
+            }
+        }
+
+        private void createTape2()
+        {
+            int n = 3;
+            int lSize = 35;
+
+            TextBox[] slots = new TextBox[n];
+
+            for (int i = 0; i < slots.Length; i++)
+            {
+                slots[i] = new TextBox();
+                slots[i].Size = new Size(lSize, lSize);
+                slots[i].TextAlign = HorizontalAlignment.Center;
+                slots[i].Font = new Font(slots[i].Font.Name, 18F);
+                slots[i].Name = "n" + i;
+                slots[i].MaxLength = 1;
+                slots[i].Text = "" + i;
+                Tape2.Controls.Add(slots[i]);
+
+                slots[i].Click += new EventHandler(slots_clickas);
+            }
+        }
+
+        void label_clickas(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                clickedLabel.BackColor = Color.Red;
+            }
+        }
+
+        void slots_clickas(object sender, EventArgs e)
+        {
+            TextBox clickedSlot = sender as TextBox;
+
+            if (clickedSlot != null)
+            {
+                clickedSlot.BackColor = Color.Red;
+            }
+        }
+
     }
 }
+
